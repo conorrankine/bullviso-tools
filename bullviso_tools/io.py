2f9aceec741e449e3bd08c05e30aea30b4e04a83
Conor Rankine <206134505+conorrankine@users.noreply.github.com>
2025-12-19 22:39:39 +0000

    added the `OutputConfig` dataclass to `io.py`; defined basic output configurations for the XTB and ORCA computational chemical packages

--- bullviso_tools/io.py
+++ bullviso_tools/io.py
@@ -7,8 +7,30 @@
 from __future__ import annotations
 
 import os
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Iterator
+
+# =============================================================================
+#                                   CLASSES
+# =============================================================================
+
+@dataclass(frozen = True)
+class OutputConfig:
+
+    xyz_f_glob: str
+    out_f_glob: str
+
+OUTPUT_CONFIGS: dict[str, OutputConfig] = {
+    'xtb': OutputConfig(
+        xyz_f_glob = 'xtbopt.xyz',
+        out_f_glob = 'xtb.out'
+    ),
+    'orca': OutputConfig(
+        xyz_f_glob = '0*[!j].xyz',
+        out_f_glob = '0*.out'
+    ),
+}
 
 # =============================================================================
 #                                  FUNCTIONS
