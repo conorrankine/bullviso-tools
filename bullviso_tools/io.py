c8210850ed6198a16d3ac93dea0ee7770fbfffe6
Conor Rankine <206134505+conorrankine@users.noreply.github.com>
2025-12-19 23:27:40 +0000

    added the `get_scf_energy()` function to `io.py`

--- bullviso_tools/io.py
+++ bullviso_tools/io.py
@@ -75,7 +75,7 @@ def detect_output_type(
     result_d: Path
 ) -> str:
     """
-    Return the output type matching the files contained in the specified
+    Returns the output type matching the files contained in the specified
     result directory; the output type is the key in `OUTPUT_CONFIGS` whose glob
     patterns (`xyz_f_glob` and `out_f_glob`) both resolve succesfully.
 
@@ -103,11 +103,64 @@ def detect_output_type(
         )
     if len(matches) > 1:
         raise ValueError(
-            f'mutliple possible output types detected for {result_d}: '
+            f'multiple possible output types detected for {result_d}: '
             f'candidates = {{{", ".join(matches)}}}'
         )
 
     return matches[0]
+
+def get_scf_energy(
+    result_d: Path,
+    output_config: OutputConfig | None = None
+) -> float:
+    """
+    Returns the SCF energy in Hartree extracted from the output file contained
+    in the specified result directory.
+
+    Args:
+        result_d (Path): Result directory.
+        output_config (OutputConfig, optional): Output configuration to guide
+            parsing of the output file; if None, an output configuration is
+            inferred from the files in the specified result directory.
+
+    Returns:
+        float: SCF energy (Hartree).
+
+    Raises:
+        FileNotFoundError: If an output file cannot be found;
+        ValueError: If multiple possible output files are found;
+        ValueError: If the SCF energy cannot be found in the output file.
+    """
+    
+    output_config = (
+        output_config or OUTPUT_CONFIGS[detect_output_type(result_d)]
+    )
+    
+    out_files = list(result_d.glob(output_config.out_f_glob))
+    if not out_files:
+        raise FileNotFoundError(
+            f'no output files matching \'{output_config.out_f_glob}\' '
+            f'found in {result_d}'
+        )
+    if len(out_files) > 1:
+        raise ValueError(
+            f'multiple output files matching \'{output_config.out_f_glob}\' '
+            f'found in {result_d}: matches = {{{", ".join(out_files)}}}'
+        )
+
+    out_f = out_files[0]
+    energy_line_config = output_config.energy_line_config
+    with out_f.open('r') as f:
+        for line in f:
+            if energy_line_config.flag in line:
+                parts = line.strip().split()
+                if not parts:
+                    continue
+                return float(parts[energy_line_config.target_index])
+    raise ValueError(
+        f'couldn\'t get the SCF energy from {out_f}; no lines containing the '
+        f'target string flag \'{energy_line_config.flag}\''
+    )
 
 # =============================================================================
 #                                     EOF
